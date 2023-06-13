3f37065f610c4af97c8317793f564419e7eb798d
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-06-13 13:09:30 -0300

    303-1: it should not be able to a user create another user

--- tests/Feature/User/CreateTest.php
+++ tests/Feature/User/CreateTest.php
@@ -2,37 +2,40 @@
 
 use App\Models\{Role, User};
 
-use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, assertDatabaseMissing, post};
 
 it('should be able to a admin create a user', function () {
     $this->seed();
 
     $admin = User::find(1);
-    $user  = User::factory()->create();
 
     actingAs($admin);
 
     post(route('user.store'), [
-        'name'                  => 'Test Admin',
-        'email'                 => 'admin@example.com',
-        'password'              => 'password',
-        'password_confirmation' => 'password',
-        'role_id'               => Role::ADMIN,
-    ])->assertRedirect();
-
-    post(route('user.store'), [
         'name'                  => 'Test User',
-        'email'                 => 'user@example.com',
+        'email'                 => 'test@example.com',
         'password'              => 'password',
         'password_confirmation' => 'password',
         'role_id'               => Role::USER,
     ])->assertRedirect();
 
+    assertDatabaseCount('users', 2);
+    assertDatabaseHas('users', [
+        'name'    => 'Test User',
+        'role_id' => 2,
+    ]);
+});
+
+it('should not be able to a user create another user', function () {
+    $this->seed();
+
+    $user = User::factory()->create();
+
     actingAs($user);
 
     $request = post(route('user.store'), [
-        'name'                  => 'Test User 2',
-        'email'                 => 'user2@example.com',
+        'name'                  => 'Test User',
+        'email'                 => 'test@example.com',
         'password'              => 'password',
         'password_confirmation' => 'password',
         'role_id'               => Role::USER,
@@ -40,13 +43,9 @@ it('should be able to a admin create a user', function () {
 
     $request->assertForbidden();
 
-    assertDatabaseCount('users', 4);
-    assertDatabaseHas('users', [
-        'name'    => 'Test Admin',
-        'role_id' => 1,
-    ]);
-    assertDatabaseHas('users', [
+    assertDatabaseCount('users', 2);
+    assertDatabaseMissing('users', [
         'name'    => 'Test User',
-        'role_id' => 2,
+        'role_id' => 1,
     ]);
 });

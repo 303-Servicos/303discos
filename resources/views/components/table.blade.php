e2e484e63f1f455a85ca5808a1bc2b86af2edbfa
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-06-15 17:30:42 -0300

    303-1: wip

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,5 +1,6 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400" {{ $attributes }}>
         {{ $slot }}
     </table>
 </div>
+

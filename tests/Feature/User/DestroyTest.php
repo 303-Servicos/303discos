01292f89f4bf08bd2f2bed814e8b997503e94e94
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-06-16 13:14:00 -0300

    303-1: it should not be able to a manager destroy a user

--- tests/Feature/User/DestroyTest.php
+++ tests/Feature/User/DestroyTest.php
@@ -1,19 +1,19 @@
 <?php
 
 use App\Models\{Role, User};
+use Database\Seeders\RoleSeeder;
 
 use function Pest\Laravel\{actingAs, assertDatabaseMissing, delete, post};
 
 test('only authenticated users can access delete', function () {
-    $this->seed();
-
+    $this->seed(RoleSeeder::class);
     $user = User::factory()->create();
 
     delete(route('users.destroy', $user))->assertRedirect('login');
 });
 
 it('should be able to a admin destroy a user', function () {
-    $this->seed();
+    $this->seed(RoleSeeder::class);
     $admin = User::factory()->create(['role_id' => Role::ADMIN]);
     $user  = User::factory()->create();
 
@@ -26,12 +26,20 @@ it('should be able to a admin destroy a user', function () {
     ]);
 });
 
+it('should not be able to a manager destroy a user', function () {
+    $this->seed(RoleSeeder::class);
+    $manager = User::factory()->create(['role_id' => Role::MANAGER]);
+    $user    = User::factory()->create();
+
+    actingAs($manager);
+    delete(route('users.destroy', $user))->assertForbidden();
+});
+
 it('should not be able to a user destroy a user', function () {
-    $this->seed();
+    $this->seed(RoleSeeder::class);
     $user  = User::factory()->create(['role_id' => Role::USER]);
     $user2 = User::factory()->create(['role_id' => Role::USER]);
 
     actingAs($user);
-
     delete(route('users.destroy', $user2))->assertForbidden();
 });

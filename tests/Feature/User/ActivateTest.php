4453dd17e1e8cae9aeed2885e8ca3f75ac566cf6
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-06-16 19:58:49 -0300

    303-1: it should not be able to a user activate another user

--- tests/Feature/User/ActivateTest.php
+++ tests/Feature/User/ActivateTest.php
@@ -45,3 +45,12 @@ it('should be able to a manager activate a user', function () {
         'is_active' => false,
     ]);
 });
+
+it('should not be able to a user activate another user', function () {
+    $this->seed(RoleSeeder::class);
+    $user  = User::factory()->create();
+    $user2 = User::factory()->create(['name' => 'Test User', 'is_active' => false]);
+
+    actingAs($user);
+    put(route('users.activate', $user2))->assertForbidden();
+});

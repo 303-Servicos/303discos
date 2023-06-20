ee232bbfa04f33eeeecb66c07019ce782e0b9deb
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-06-20 01:52:06 -0300

    303-1: it should be able to a manage create a label

--- tests/Feature/Label/StoreTest.php
+++ tests/Feature/Label/StoreTest.php
@@ -31,27 +31,21 @@ it('should be able to a admin create a label', function () {
     ]);
 });
 
-//it('should not be able to a manage create a label', function () {
-//    seed(RoleSeeder::class);
-//    $manager = User::factory()->create(['role_id' => Role::MANAGER]);
-//
-//    actingAs($manager);
-//
-//    $request = post(route('labels.store'), [
-//        'name'                  => 'Test User',
-//        'email'                 => 'test@example.com',
-//        'password'              => 'password',
-//        'password_confirmation' => 'password',
-//        'role_id'               => Role::USER,
-//    ]);
-//
-//    $request->assertForbidden();
-//
-//    assertDatabaseMissing('labels', [
-//        'name'    => 'Test User',
-//        'role_id' => Role::USER,
-//    ]);
-//});
+it('should be able to a manage create a label', function () {
+    seed(RoleSeeder::class);
+    $manager = User::factory()->create(['role_id' => Role::MANAGER]);
+
+    actingAs($manager);
+
+    post(route('labels.store'), [
+        'name'    => 'Test Label',
+        'discogs' => 'www.discogs.com',
+    ])->assertRedirect();
+
+    assertDatabaseHas('labels', [
+        'name' => 'Test Label',
+    ]);
+});
 
 //it('should not be able to a label create a label', function () {
 //    seed(RoleSeeder::class);
